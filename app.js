c3bd5b02527b5855cc337427bdcbd059a8f14d77
Ahmad A. Fouad <a.adel.dev@gmail.com>
2023-05-28 20:10:43 +0300

    game is complete

--- app.js
+++ app.js
@@ -5,53 +5,122 @@ const Player = (name, symbol) => {
   return { getName, getSymbol };
 };
 
-const game = (() => {
-  let gameBoard = [];
-  const addSlotToGameBoard = (slot) => gameBoard.push(slot);
-  const displayGameBoard = () => {
-    gameBoard.forEach((slot) => {
-      console.log(slot);
-    });
+const slot = (mark) => {
+  const getMark = () => mark;
+  const setMark = (symbol) => (mark = symbol);
+  const canModify = () => {
+    if (mark === "") return true;
+    else return false;
   };
+
+  return {
+    getMark,
+    canModify,
+    setMark,
+  };
+};
+
+const game = (() => {
+  const gameBoard = [];
+  let gameOver = false;
+  const addSlotToGameBoard = (slot) => gameBoard.push(slot);
   const playerOne = Player("playerOne", "X");
   const playerTwo = Player("playerTwo", "O");
   let currentPlayer = playerOne;
   const setCurrentPlayer = (player) => (currentPlayer = player);
 
-  const checkGame = (slot) => {
-    slot.innerHTML = `<h1>${currentPlayer.getSymbol()}</h1>`;
+  const createGameBoard = () => {
+    for (let i = 0; i < 9; i++) {
+      const slotInstance = slot("");
+      addSlotToGameBoard(slotInstance);
+    }
+  };
+
+  const renderGameBoard = (gameBoardDisplay) => {
+    gameBoardDisplay.innerHTML = "";
+    gameBoard.forEach((slot) => {
+      const slotDisplay = document.createElement("div");
+      slotDisplay.className = "slot";
+      slotDisplay.innerHTML = `<h1>${slot.getMark()}</h1>`;
+      slotDisplay.addEventListener("mouseover", () => {
+        slotDisplay.classList.add("highlight");
+      });
+      slotDisplay.addEventListener("mouseleave", () => {
+        slotDisplay.classList.remove("highlight");
+      });
+      if (slot.canModify()) {
+        slotDisplay.addEventListener("click", () => {
+          updateGame(slot);
+        });
+      }
+      gameBoardDisplay.append(slotDisplay);
+    });
+  };
+
+  const switchActivePlayer = () => {
     if (currentPlayer === playerOne) {
       setCurrentPlayer(playerTwo);
     } else {
       setCurrentPlayer(playerOne);
     }
-    console.log(currentPlayer.getName());
+  };
+
+  const deactivateGameBoard = () => {
+    gameOver = true;
+  };
+
+  const updateGame = (slot) => {
+    if (gameOver === true) return;
+    slot.setMark(currentPlayer.getSymbol());
+    renderGameBoard(gameBoardDisplay);
+    if (checkWinCondition(currentPlayer) != null) {
+      deactivateGameBoard(gameBoardDisplay);
+    } else {
+      switchActivePlayer();
+    }
+  };
+
+  const checkWinCondition = (player) => {
+    const winningCombinations = [
+      [0, 1, 2], // Rows
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6], // Columns
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8], // Diagonals
+      [2, 4, 6],
+    ];
+
+    for (let combination of winningCombinations) {
+      const [a, b, c] = combination;
+      const slotA = gameBoard[a];
+      const slotB = gameBoard[b];
+      const slotC = gameBoard[c];
+
+      if (
+        slotA.getMark() !== "" &&
+        slotA.getMark() === slotB.getMark() &&
+        slotB.getMark() === slotC.getMark()
+      ) {
+        if (slotA.getMark() === playerOne.getSymbol()) {
+          return playerOne;
+        } else {
+          return playerTwo;
+        }
+      }
+    }
+
+    return null;
   };
 
   return {
-    addSlotToGameBoard,
-    displayGameBoard,
-    checkGame,
+    renderGameBoard,
+    createGameBoard,
   };
 })();
 
-const gameBoard = document.getElementById("game-board");
+const gameBoardDisplay = document.getElementById("game-board");
 
-for (let i = 0; i < 9; i++) {
-  const slot = document.createElement("div");
-  slot.className = "slot";
-  slot.id = `slot${i}`;
-  slot.addEventListener("mouseover", () => {
-    slot.classList.add("highlight");
-  });
-  slot.addEventListener("mouseleave", () => {
-    slot.classList.remove("highlight");
-  });
-  game.addSlotToGameBoard(slot);
-  slot.addEventListener("click", () => {
-    game.checkGame(slot);
-  });
-  gameBoard.appendChild(slot);
-}
-
-//console.log(game.displayGameBoard());
+game.createGameBoard();
+game.renderGameBoard(gameBoardDisplay);
